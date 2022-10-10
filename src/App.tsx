bb1067ad35e3c11d2225aec7707b17c05cebbb93
Ruveendra Dewshan <ruveendradeewshan@gmail.com>
2022-10-10 07:47:36 +0530

    Socket Connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,130 @@
-import React from 'react';
-import { Plate } from '@udecode/plate';
+import React, { useMemo, useRef,useEffect} from "react";
+//import { Plate } from '@udecode/plate';
 import { balloonToolbarValue } from './balloonToolbarValue';
 import { MarkBalloonToolbar } from './MarkBalloonToolbar';
 import { basicNodesPlugins } from './basicNodesPlugins';
 import { editableProps } from './editableProps';
-import { MyValue } from './plateTypes';
+import { MyValue } from './plateTypes'
+import {
+  createPlateEditor,
+  usePlateStates,
+  Plate,
+} from '@udecode/plate';
+import {
+  SyncElement,
+  toSharedType,
+  useCursors,
+  withCursor,
+  withYjs,
+} from "slate-yjs";
+import { WebsocketProvider } from "y-websocket";
+import { withReact } from "slate-react";
+import { withHistory } from "slate-history";
+import * as Y from "yjs";;
 
-export default () => (
-  <Plate<MyValue>
+const App =() => {
+
+    const [value, setValue] = usePlateStates().value();
+    const [sharedType, provider] = useMemo(() => {
+    const doc = new Y.Doc();
+    const  sharedType = doc.getArray<SyncElement>("content");
+    const provider = new WebsocketProvider("ws://127.0.0.1:1234", "slate-yjs-demo", doc, {
+      connect: false,
+    });
+  
+    return [sharedType, provider];
+  }, []);
+  
+  
+  
+  const editor:any = useMemo(() => {
+    const editor:any = withCursor(
+      withYjs(withReact(withHistory(createPlateEditor())), sharedType),
+      provider.awareness
+    );
+  
+    return editor;
+  }, [sharedType, provider]);
+  
+  useEffect(() => {
+    // provider.on("status", ({ status }) => {
+    //   setOnlineState(status === "connected");
+    // });
+  
+    // provider.awareness.setLocalState({
+    //   alphaColor: color.slice(0, -2) + "0.2)",
+    //   color,
+    //   // name,
+    // });
+  
+    // Super hacky way to provide a initial value from the client, if
+    // you plan to use y-websocket in prod you probably should provide the
+    // initial state from the server.
+    provider.on("sync", (isSynced: boolean) => {
+      if (isSynced && sharedType.length === 0) {
+        toSharedType(sharedType, [
+          { children: [{ text: "Hello world!" }] },
+        ]);
+      }
+    });
+
+    provider.on("sync", (isSynced: boolean) => {
+      if (isSynced && sharedType.length === 0) {
+        toSharedType(sharedType, [
+          { children: [{ text: balloonToolbarValue }] },
+        ]);
+      }
+    });
+  
+    provider.connect();
+  
+    return () => {
+      provider.disconnect();
+    };
+  }, [provider]);
+
+  const toggleOnline = () => {
+    provider.connect();
+    
+  };
+
+  const toggleOffline = () => {
+    provider.disconnect() 
+  };
+
+  return(
+    <>
+     <Plate<MyValue>
+    //editor={editor}
     editableProps={editableProps}
     plugins={basicNodesPlugins}
-    initialValue={balloonToolbarValue}
+    //initialValue={balloonToolbarValue}
   >
     <MarkBalloonToolbar />
   </Plate>
-);
 
+  <div >
+        <button type="button" onClick={toggleOnline}>
+          Go Online
+        </button>
+
+      </div>
+
+  <div >
+        <button type="button" onClick={toggleOffline}>
+          Go Offline
+        </button>
+
+      </div>
+    </>
+
+   
+  
+  )
+  
+};
+
+export default App;
 //..............with Styles...............
 
 // import React, { useMemo, useRef,useEffect} from "react";
